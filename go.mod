b19a6554ca365d1a49e157e76d1d29b08735af5d
subuta <shunta.3110@gmail.com>
2021-10-15 17:58:05 +0900

    Add initial files.

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/subuta/play-with-go-generate
+
+go 1.17
+
+require (
+	github.com/k0kubun/pp v3.0.1+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"github.com/k0kubun/pp"
+	"golang.org/x/tools/imports"
+	"log"
+	"os"
+	"text/template"
+)
+
+// FormatSource is gofmt with addition of removing any unused imports.
+// SEE: https://github.com/webrpc/webrpc/blob/v0.6.0/gen/golang/helpers.go
+func FormatSource(source []byte) ([]byte, error) {
+	return imports.Process("", source, &imports.Options{
+		AllErrors: true, Comments: true, TabIndent: true, TabWidth: 8,
+	})
+}
+
+func main() {
+	funcMap := template.FuncMap{
+		"GetGreet": GetGreet,
+	}
+
+	tmpl, err := template.New("sample").
+		Funcs(funcMap).
+		ParseFiles("gen/golang/templates/main.go.tmpl")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// generate the template
+	genBuf := bytes.NewBuffer(nil)
+	err = tmpl.ExecuteTemplate(genBuf, "main.go.tmpl", struct{}{})
+	if err != nil {
+		pp.Println(err)
+	}
+
+	src, err := FormatSource(genBuf.Bytes())
+	if err != nil {
+		pp.Println(err)
+	}
+
+	err = os.WriteFile("generated-main.go", src, 0644)
+	if err != nil {
+		pp.Println(err)
+	}
+}
+
+func GetGreet() string {
+	return "Hello world!"
+}
